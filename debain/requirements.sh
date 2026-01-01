690de2155afeb78436de2d01079177d37f5da8d0
Hussam1x <125204349+Hussam1x@users.noreply.github.com>
2026-01-01 17:51:42 +0200

    Add files via upload

---
+++ debain/requirements.sh
@@ -0,0 +1,6 @@
+sudo apt-get install xterm
+sudo apt-get install arpspoof
+sudo apt-get install arp-scan
+sudo apt-get install dsniff
+sudo apt-get update && sudo apt-get install net-tools
+sudo apt-get upgrade net-tools
