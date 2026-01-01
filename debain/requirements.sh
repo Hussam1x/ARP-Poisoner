525a2ba0f3af0e0f844e92bf6b1ddab655d37eb4
Hussam1x <125204349+Hussam1x@users.noreply.github.com>
2026-01-01 18:19:01 +0200

    Delete debain directory

--- debain/requirements.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install xterm
-sudo apt-get install arpspoof
-sudo apt-get install arp-scan
-sudo apt-get install dsniff
-sudo apt-get update && sudo apt-get install net-tools
-sudo apt-get upgrade net-tools
